b51470f28bf06bdb2d5106593c436f29035baba9
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-23 13:43:15 +0700

    feat: persistent store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import reducer from "./reducers";
 
-const store = configureStore({
+const persistedReducer = persistReducer(
+	{
+		key: "todo",
+		storage,
+	},
 	reducer,
-});
+);
+
+const store = configureStore(persistedReducer);
 
 export default store;
+
+export const persistor = persistStore(store);
